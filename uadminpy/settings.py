2909fed58f8911734722a3f8a8f861d6cd95e23c
Ygam Retuta <ygam.j.retuta@gmail.com>
2014-10-10 16:49:24 +0800

    fix tuple error for STATICFILES_DIRS

--- uadminpy/settings.py
+++ uadminpy/settings.py
@@ -86,7 +86,7 @@ USE_TZ = True
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static'),
 )
 
 MEDIA_ROOT = 'uploads'
