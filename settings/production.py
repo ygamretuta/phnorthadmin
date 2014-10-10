6d12340c1c3ede792d34ca9fa1eecb36464c8594
Ygam Retuta <ygam.j.retuta@gmail.com>
2014-10-10 20:25:18 +0800

    one true way settings

--- settings/production.py
+++ settings/production.py
@@ -1,14 +1,13 @@
 from settings.base import *
 
-DEBUG = False
-TEMPLATE_DEBUG = False
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
 
 import dj_database_url
 DATABASES = {
     'default': dj_database_url.config()
 }
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-ALLOWED_HOSTS = ['*']
 
 HEROKU_BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATICFILES_DIRS = (
